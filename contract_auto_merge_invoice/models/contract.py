1bad761daa9cbb0d8537beb949e7308cbb8dee05
Florent Cayré <florent@commown.coop>
2022-09-19 20:34:54 +0200

    Setup partner payment preferred date before emitting a contract invoice
    This is done each time a contract invoice is emitted when the partner
    preferred payment date is not already set, so should occur only once.

--- contract_auto_merge_invoice/models/contract.py
+++ contract_auto_merge_invoice/models/contract.py
@@ -1,12 +1,35 @@
 from odoo import api, models
 
 
+def _key_order_by_recurrence(cline):
+    "Function used to sort contract lines by recurrence duration"
+    return cline.get_relative_delta(cline.recurring_rule_type, cline.recurring_interval)
+
+
 class Contract(models.Model):
     _inherit = "contract.contract"
 
     @api.multi
     def _prepare_invoice(self, *args, **kwargs):
-        "Override contract invoice creation to add auto_merge=True"
+        """Override invoice creation to setup payment at partner's chosen date
+        - add auto_merge=True
+        - fill-in the partner's auto-payment data
+
+        Note that if contract is_auto_pay is True, this has no effect
+        on its invoices.
+        """
         vals = super(Contract, self)._prepare_invoice(*args, **kwargs)
         vals["auto_merge"] = True
+
+        for contract in self:
+            if not contract.partner_id.invoice_merge_next_date:
+                cline = contract.contract_line_ids.sorted(key=_key_order_by_recurrence)
+                contract.partner_id.update(
+                    {
+                        "invoice_merge_next_date": vals["date_invoice"],
+                        "invoice_merge_recurring_rule_type": cline.recurring_rule_type,
+                        "invoice_merge_recurring_interval": cline.recurring_interval,
+                        "auto_merge_invoice": True,
+                    }
+                )
         return vals
